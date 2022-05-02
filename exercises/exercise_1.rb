6e3e71d61481bfb2f1b4e880cf97bcf8195b4c57
Ryan Kwok <kaj.kwok@gmail.com>
2022-05-02 14:25:31 -0700

    create stores via ex1, update store name of burnaby to Downtown Vancouver in ex2, deleted Gastown store

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,17 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+class Store < ActiveRecord::Base
+  # def initialize(name, annual_revenue, mens_apparel, womens_apparel)
+  #   @name = name 
+  #   @annual_revenue = annual_revenue
+  #   @mens_apparel = mens_apparel
+  #   @womens_apparel = womens_apparel
+  # end
+end
+
+Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: "true", womens_apparel: "true")
+Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: "false", womens_apparel: "true")
+Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: "true", womens_apparel: "false")
+
+puts Store.count
