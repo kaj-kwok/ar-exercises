da679dd1037879ff9765d4ecc36977473e82f8cd
Ryan Kwok <kaj.kwok@gmail.com>
2022-05-02 14:49:34 -0700

    added queries to output total revenue / avg revenue / output count of stores than generate > 1mil

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+#total annual revenue
+puts Store.sum("annual_revenue")
+
+#average annual revenue
+puts Store.average("annual_revenue")
+
+#Output the number of stores that are generating $1M or more in annual sales.
+puts Store.where(["annual_revenue > ?", 1000000]).count
