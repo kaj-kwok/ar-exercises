ae1d600f9208bf5b31efead6e31e953ff25e3321
Ryan Kwok <kaj.kwok@gmail.com>
2022-05-02 17:44:10 -0700

    ex7 added validation steps

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,30 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+class Store < ActiveRecord::Base
+  has_many :employees
+  validates :name, length: { minimum: 3 } 
+  validates :annual_revenue, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+  validates :mens_apparel, :womens_apparel, inclusion: [true, false]
+  validate :must_have_stock, on: :create
+
+  def must_have_stock
+    if mens_apparel === false && womens_apparel === false
+      errors.add(:base, "both mens_apparel & womens_apparel must not be false")
+    end
+  end
+end
+
+class Employee < ActiveRecord::Base
+  belongs_to :store
+  validates :first_name, :last_name, :store_id, presence: true 
+  validates :hourly_rate, numericality: {only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200}
+  validates_associated :store
+end
+
+store = gets.chomp
+x = Store.create(name: store)
+x.errors.full_messages.each do |message|
+  puts "#{message}"
+end
